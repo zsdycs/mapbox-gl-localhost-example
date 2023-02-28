b294bc0f5ec18bfc4988cecbd8fbb9095d7d72d5
李鹏坤 <mail@lipk.org>
2023-02-28 16:22:15 +0800

    change: 修改模拟数据

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -38,8 +38,8 @@
       </div>
     </div>
     <button class="btn" @click="updatedMapBoxComponentData()">更新数据</button>
-    <!-- <button class="btn" @click="startRecording()">开始记录数据</button>
-    <button class="btn" @click="endRecording()">结束记录数据</button> -->
+    <button class="btn" @click="startRecording()">开始记录数据</button>
+    <button class="btn" @click="endRecording()">结束记录数据</button>
   </div>
 </template>
 
@@ -137,10 +137,10 @@ export default {
     }, 1000);
 
     // 添加地图中心点标记
-    // this.$refs.mapBoxRef.$el.insertAdjacentHTML(
-    //   'afterbegin',
-    //   `<div class="canter-mark"></div>`
-    // );
+    this.$refs.mapBoxRef.$el.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="canter-mark"></div>`
+    );
   },
   updated() { },
   destroyed() {
@@ -335,6 +335,7 @@ export default {
           Math.floor(Math.random() * 10) * 0.001 * plusOrMinus,
         ]);
       }
+      // console.log('======= coordinates ========', JSON.stringify(coordinates));
       this.mapBoxComponent_mapInfoMarkGeojson = makeExampleMockGeojson({
         placeInfos,
         coordinates,
@@ -417,7 +418,7 @@ export default {
           'line-cap': 'round',
         },
         paint: {
-          'line-color': '#c6ab00',
+          'line-color': '#aa3d1c',
           'line-width': 8,
         },
       });
